2e96b79ba3633f86badf087eb71c1ca5c040799a
Your-Dad-Codes <122411567+Your-Dad-Codes@users.noreply.github.com>
2023-02-27 22:47:55 -0500

    1111

---
+++ pactice2/js/app.js
@@ -0,0 +1,14 @@
+
+// function square(number) {
+//     return number * number;
+// }
+
+// //Save value in html
+// let result= square(4368);
+// document.getElementById("demo").
+// // innerHTML = result;
+
+let weather= "Partly Cloudy";
+let mydate = 27; mydate+1
+let temperature; temperature=78.2
+

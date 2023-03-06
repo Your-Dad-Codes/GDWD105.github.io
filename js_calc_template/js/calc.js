503af2602be57f8cb07ceebb63b34128627f15ed
Your-Dad-Codes <122411567+Your-Dad-Codes@users.noreply.github.com>
2023-03-06 18:08:53 -0500

    12345t

--- js_calc_template/js/calc.js
+++ js_calc_template/js/calc.js
@@ -2,7 +2,7 @@ console.log ('HI')
 
 let answer = document.getElementById('answer');
 
-function twoPlusTwo(n1, n2) {
+function twoPlusTwo() {
     answer.innerHTML = 2 + 2;
 }
 function multiply(n1, n2) {
@@ -14,8 +14,8 @@ function divide(n1, n2) {
 }
 
 function multiplyTwoNumbers() {
-    let firstNumber = document.getElementById ('FirstNumber');
-    let secondNumber = document.getElementById ('SecondNumber');
+    let firstNumber = document.getElementById('FirstNumber');
+    let secondNumber = document.getElementById('SecondNumber');
     let solve = firstNumber.value * secondNumber.value;
     answer.innerHTML = solve;
 }

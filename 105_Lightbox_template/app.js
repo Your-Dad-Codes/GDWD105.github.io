320f91437fcbe1a12d5ec783153cee4490ac7571
Your-Dad-Codes <122411567+Your-Dad-Codes@users.noreply.github.com>
2023-03-06 20:59:23 -0500

    qwert

--- 105_Lightbox_template/app.js
+++ 105_Lightbox_template/app.js
@@ -12,19 +12,37 @@ function openlightbox() {
     lightbox.src =this.src;
 }
 
+function closelightbox() {
+    lightboxContainer.classList.remove('display')
+    lightbox.src=""
+}
 // get reference
 
-let coffeeImage = document.getElementById('coffimg')
-let coffeeImage2 = document.getElementById('coffimg2')
-let coffeeImage3 = document.getElementById('coffimg3')
-let coffeeImage4 = document.getElementById('coffimg4')
-let coffeeImage5 = document.getElementById('coffimg5')
-let coffeeImage6 = document.getElementById('coffimg6')
-let coffeeImage7 = document.getElementById('coffimg7')
-let coffeeImage8 = document.getElementById('coffimg8')
-let coffeeImage9 = document.getElementById('coffimg9')
-let coffeeImage10 = document.getElementById('coffimg10')
-let coffeeImage11 = document.getElementById('coffimg11')
+let coffeeImage = document.getElementById('coffeeImage');
+let coffeeImage2 = document.getElementById('coffeeImage2');
+let coffeeImage3 = document.getElementById('coffeeImage3');
+let coffeeImage4 = document.getElementById('coffeeImage4');
+let coffeeImage5 = document.getElementById('coffeeImage5');
+let coffeeImage6 = document.getElementById('coffeeImage6');
+let coffeeImage7 = document.getElementById('coffeeImage7');
+let coffeeImage8 = document.getElementById('coffeeImage8');
+let coffeeImage9 = document.getElementById('coffeeImage9');
+let coffeeImage10 = document.getElementById('coffeeImage10');
+let coffeeImage11 = document.getElementById('coffeeImage11');
 // attach the openlighbox function to the image
 
 coffeeImage.onclick = openlightbox;
+coffeeImage2.onclick = openlightbox;
+coffeeImage3.onclick = openlightbox;
+coffeeImage4.onclick = openlightbox;
+coffeeImage5.onclick = openlightbox;
+coffeeImage6.onclick = openlightbox;
+coffeeImage7.onclick = openlightbox;
+coffeeImage8.onclick = openlightbox;
+coffeeImage9.onclick = openlightbox;
+coffeeImage10.onclick = openlightbox;
+coffeeImage11.onclick = openlightbox;
+// light box closer//
+
+lightboxCloser.onclick = closelightbox;
+

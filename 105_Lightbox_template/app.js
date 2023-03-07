af6257f788c8a4f05d112f947377e71103aaad30
Your-Dad-Codes <122411567+Your-Dad-Codes@users.noreply.github.com>
2023-03-06 20:48:15 -0500

    awert

--- 105_Lightbox_template/app.js
+++ 105_Lightbox_template/app.js
@@ -2,7 +2,7 @@
 
 let lightboxContainer = document.getElementById('lightboxContainer');
 let lightboxBackground = document.getElementById('lightboxBackground');
-let Lightbox = document.getElementById('lightbox');
+let lightbox = document.getElementById('lightbox');
 let lightboxCloser = document.getElementById('lightboxCloser');
 
 // create function
@@ -14,8 +14,17 @@ function openlightbox() {
 
 // get reference
 
-let coffeeImage = document.getElementById('coffeeImage')
-
+let coffeeImage = document.getElementById('coffimg')
+let coffeeImage2 = document.getElementById('coffimg2')
+let coffeeImage3 = document.getElementById('coffimg3')
+let coffeeImage4 = document.getElementById('coffimg4')
+let coffeeImage5 = document.getElementById('coffimg5')
+let coffeeImage6 = document.getElementById('coffimg6')
+let coffeeImage7 = document.getElementById('coffimg7')
+let coffeeImage8 = document.getElementById('coffimg8')
+let coffeeImage9 = document.getElementById('coffimg9')
+let coffeeImage10 = document.getElementById('coffimg10')
+let coffeeImage11 = document.getElementById('coffimg11')
 // attach the openlighbox function to the image
 
 coffeeImage.onclick = openlightbox;
